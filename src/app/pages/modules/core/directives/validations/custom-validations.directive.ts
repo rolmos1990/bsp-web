1b0461ca2ddd878f27a21ddb15f9d77f8f46b28c
Vladimir Blanco <vblanco@labequis.com>
2019-04-17 15:53:37 -0400

    cambios de validacion

--- src/app/pages/modules/core/directives/validations/custom-validations.directive.ts
+++ src/app/pages/modules/core/directives/validations/custom-validations.directive.ts
@@ -1,5 +1,6 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 import { Directive } from '@angular/core';
+import * as moment from 'moment';
 
 @Directive({
     selector: '[appCustomValidator]',
@@ -24,8 +25,20 @@ export class CustomValidatorDirective {
      */
     static dateValidator(control: AbstractControl): ValidationErrors {
         const date = /([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))/;
-        if (control.value && !date.test(control.value)) {
+        const _date = moment(control.value);
+        if (control.value && (!date.test(control.value) || _date.isAfter(moment.now(), 'day'))) {
             return { invalidDate: true };
+        }
+    }
+
+    /**
+     * Validate that the FormControl has this structure "+507 xxxx xxxx".
+     * @param control FormControl to evaluate.
+     */
+    static regularText(control: AbstractControl): ValidationErrors {
+        const text = /((^[A-z\s\,\.]{2,50})$)/g;
+        if (control.value && !text.test(control.value)) {
+            return { invalidText: true };
         }
     }
 
