7da90f4bcf70200986e0931c8ccd07bbcd234b89
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2019-04-23 16:08:27 -0400

    Comitiando ando antes de hacer el merge con development

---
+++ src/app/pages/modules/apinfo/pages/show/show.apinfo.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'bsp-show',
+  templateUrl: './show.apinfo.html',
+  styleUrls: ['./show.apinfo.scss']
+})
+export class ShowApinfo implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
