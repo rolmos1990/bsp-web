5fb1d239723627efb68da0209a1ff4a13c192af3
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2019-04-23 23:13:50 -0400

    Subiendo pagina nueva

--- src/app/pages/modules/apinfo/pages/show/show.apinfo.ts
+++ src/app/pages/modules/apinfo/pages/show/show.apinfo.ts
@@ -12,28 +12,63 @@ export class ShowApinfo implements OnInit {
   isFirstOpen = true;
 
   public slides = [
-    'First slide',
-    'Second slide',
-    'Third slide',
-    'Fourth slide',
-    'Fifth slide',
-    'Sixth slide'
+    { 'titulo': 'Muerte accidental',
+    'imagen' : 'unoapinfo',
+    'mensaje': 'Si el asegurado muere en un accidente, a los beneficiarios se les pagará el monto asegurado que contrató (indemnización). Ejm: accidentes de tránsito, sucesos imprevistos, homicidios dolosos y culposos.' 
+    },
+    { 'titulo': 'Adelantos de gastos funerarios',
+    'imagen' : 'dosapinfo',
+    'mensaje': 'Adelantamos hasta el 25% la suma asegurada de gastos funerarios a los beneficiarios cuando ocurre la muerte accidental del asegurado. Ejm: si tu suma asegurada por muerte accidental es $10.000 te adelantamos hasta $2500' 
+    },
+    { 'titulo': 'Invalidez permanente',
+    'imagen' : 'tresapinfo',
+    'mensaje': 'Cubrimos un porcentaje según el grado de discapacidad que pudiera presentar el asegurado a causa de un accidente sufrido' 
+    },
+    { 'titulo': 'Gastos médicos por accidente',
+    'imagen' : 'cuatroapinfo',
+    'mensaje': 'Cobertura opcional donde te reembolsamos hasta un 10% de la suma asegurada para gastos médicos incurridos por accidentes.' 
+    },
+    { 'titulo': 'Doble indemnización por muerte por accidente específico',
+    'imagen' : 'cincoapinfo',
+    'mensaje': 'Duplicamos la suma asegurada por muerte accidental mientras el asegurado se transporta en un vehículo público (autobús, metro, taxi, etc) o si se ve envuelto en un incendio en algún edificio público (hotel, museo, cine, teatro, etc)' 
+    },
+    { 'titulo': 'Asistencia en viajes',
+    'imagen' : 'seisapinfo',
+    'mensaje': 'Asistencia médica durante tu viaje, si por indicación médica debes alojarte en un hotel, cubrimos tu estancia mientras te recuperas, transporte de los familiares del asegurado en caso necesario. Si ocurriera el fallecimiento del asegurado, traemos de vuelta sus restos así como a los familiares' 
+    },
   ];
 
   public type: string = 'component';
 
   public disabled: boolean = false;
 
-
-
   public config: SwiperConfigInterface = {
     direction: 'horizontal',
-    slidesPerView: 1,
+    slidesPerView: 3,
+    spaceBetween: 30,
     keyboard: true,
     mousewheel: true,
     scrollbar: false,
     navigation: true,
-    pagination: false
+    pagination: false,
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+      640: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      }
+    }
   };
 
   private scrollbar: SwiperScrollbarInterface = {
@@ -51,7 +86,10 @@ export class ShowApinfo implements OnInit {
   @ViewChild(SwiperComponent) componentRef: SwiperComponent;
   @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
 
-  constructor() {}
+  constructor(){}
+
+  ngOnInit() {
+  }
 
   public toggleType() {
     this.type = (this.type === 'component') ? 'directive' : 'component';
