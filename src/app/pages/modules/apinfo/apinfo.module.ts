a194caf9a86728a910b3d2711e4027966c323e5a
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2019-04-23 20:37:05 -0400

    Subiendo pantalla nueva y haciendo correciones

--- src/app/pages/modules/apinfo/apinfo.module.ts
+++ src/app/pages/modules/apinfo/apinfo.module.ts
@@ -3,12 +3,30 @@ import { CommonModule } from '@angular/common';
 
 import { ApinfoRoutingModule } from './apinfo-routing.module';
 import { ShowApinfo } from './pages/show/show.apinfo';
+import { AccordionModule } from 'ngx-bootstrap/accordion';
+import { SwiperModule } from 'ngx-swiper-wrapper';
+import { SWIPER_CONFIG } from 'ngx-swiper-wrapper';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
+
+const DEFAULT_SWIPER_CONFIG: SwiperConfigInterface = {
+  direction: 'horizontal',
+  slidesPerView: 'auto'
+};
+
 
 @NgModule({
   declarations: [ShowApinfo],
   imports: [
     CommonModule,
-    ApinfoRoutingModule
+    ApinfoRoutingModule,
+    AccordionModule.forRoot(),
+    SwiperModule
+  ],
+  providers: [
+    {
+      provide: SWIPER_CONFIG,
+      useValue: DEFAULT_SWIPER_CONFIG
+    }
   ]
 })
 export class ApinfoModule { }
--- src/app/pages/modules/apinfo/pages/show/show.apinfo.ts
+++ src/app/pages/modules/apinfo/pages/show/show.apinfo.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { SwiperComponent, SwiperDirective, SwiperConfigInterface,
+  SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'bsp-show',
@@ -6,10 +8,106 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./show.apinfo.scss']
 })
 export class ShowApinfo implements OnInit {
+  customClass = 'customClass';
+  isFirstOpen = true;
 
-  constructor() { }
+  public slides = [
+    'First slide',
+    'Second slide',
+    'Third slide',
+    'Fourth slide',
+    'Fifth slide',
+    'Sixth slide'
+  ];
 
-  ngOnInit() {
+  public type: string = 'component';
+
+  public disabled: boolean = false;
+
+
+
+  public config: SwiperConfigInterface = {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: true,
+    pagination: false
+  };
+
+  private scrollbar: SwiperScrollbarInterface = {
+    el: '.swiper-scrollbar',
+    hide: false,
+    draggable: true
+  };
+
+  private pagination: SwiperPaginationInterface = {
+    el: '.swiper-pagination',
+    clickable: true,
+    hideOnClick: false
+  };
+
+  @ViewChild(SwiperComponent) componentRef: SwiperComponent;
+  @ViewChild(SwiperDirective) directiveRef: SwiperDirective;
+
+  constructor() {}
+
+  public toggleType() {
+    this.type = (this.type === 'component') ? 'directive' : 'component';
+  }
+
+  public toggleDisabled() {
+    this.disabled = !this.disabled;
+  }
+
+  public toggleDirection() {
+    this.config.direction = (this.config.direction === 'horizontal') ? 'vertical' : 'horizontal';
+  }
+
+  public toggleSlidesPerView() {
+    if (this.config.slidesPerView !== 1) {
+      this.config.slidesPerView = 1;
+    } else {
+      this.config.slidesPerView = 2;
+    }
+  }
+
+  public toggleOverlayControls() {
+    if (this.config.navigation) {
+      this.config.scrollbar = false;
+      this.config.navigation = false;
+
+      this.config.pagination = this.pagination;
+    } else if (this.config.pagination) {
+      this.config.navigation = false;
+      this.config.pagination = false;
+
+      this.config.scrollbar = this.scrollbar;
+    } else {
+      this.config.scrollbar = false;
+      this.config.pagination = false;
+
+      this.config.navigation = true;
+    }
+
+    if (this.type === 'directive') {
+      this.directiveRef.setIndex(0);
+    } else {
+      this.componentRef.directiveRef.setIndex(0);
+    }
+  }
+
+  public toggleKeyboardControl() {
+    this.config.keyboard = !this.config.keyboard;
+  }
+
+  public toggleMouseWheelControl() {
+    this.config.mousewheel = !this.config.mousewheel;
+  }
+
+  public onIndexChange(index: number) {
+    console.log('Swiper index: ', index);
   }
 
 }
