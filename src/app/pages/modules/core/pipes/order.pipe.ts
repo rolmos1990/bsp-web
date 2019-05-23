84955ab107cf457a04b0e4182cc35ec3bb29e203
Alexander <alexander_deazevedo@hotmail.com>
2019-05-22 22:24:26 -0400

    intentando hacer un pipe

---
+++ src/app/pages/modules/core/pipes/order.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sorting'
+})
+
+export class SortingCompaniesPipe implements PipeTransform {
+
+  transform(value: Array<any>, args: any[]): any[] {
+
+    let field: string = args.toString();
+    if(value == null) {
+      return null;
+    }
+
+
+    value.sort((a: any, b: any) => {
+        if (a[field] < b[field]) {
+          return -1;
+        } else if (a[field] > b[field]) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+
+      return value;
+    })
+  }
+
+}
